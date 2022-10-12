66c055a055e626d435985d8e8199d9c77291959d
YabloYescobar <joshuanewe@gmail.com>
2022-10-12 13:46:58 -0700

    v1_official

---
+++ sanity_ecommerce/src/dashboardConfig.js
@@ -0,0 +1,27 @@
+export default {
+	widgets: [
+		{
+			name: "netlify",
+			options: {
+				title: "Tenth Kingdom Lavender",
+				sites: [
+					{
+						title: "Sanity Studio",
+						apiId: "d7af2313-11b2-4398-97e1-97f9857bbdfc",
+						buildHookId:
+							"https://api.netlify.com/build_hooks/63460a974e841a636ec34ba1",
+						name: "sanity-gatsby-blog-20-studio",
+					},
+					{
+						title: "Website",
+						apiId: "d7af2313-11b2-4398-97e1-97f9857bbdfc",
+						buildHookId:
+							"https://api.netlify.com/build_hooks/63460a974e841a636ec34ba1",
+						name: "melodic-truffle-df3824",
+						url: "https://my-sanity-deployment.com",
+					},
+				],
+			},
+		},
+	],
+};
